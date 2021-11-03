f2ccae6ffa27b2278b631967760166340933faf6
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2021-11-03 14:47:11 +0530

    added suitable comments toe server.js file

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
 
+// configuration
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+// middlewares
 app.use(express.json());
 app.use(cors());
 
+// endpoints
 app.get("/", (req, res) => res.status(200).json("Hello world"));
 
+// listener
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
