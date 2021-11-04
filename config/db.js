3e1fdc97857ac9963cb93e2d162d117e2142cbff
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2021-11-04 23:37:10 +0530

    completed setup for models and routes

--- config/db.js
+++ config/db.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const DBUri = `mongodb+srv://root:${process.env.PASS}@cluster0.hltf8.mongodb.net/ecomDB?retryWrites=true&w=majority`;
+    // database connection
     const conn = await mongoose.connect(DBUri, { autoIndex: false });
     console.log(`MongoDB connected:${conn.connection.host}`);
 
+    // storage connection
     const gfsConn = await mongoose
       .createConnection(DBUri, {
         autoIndex: false,
