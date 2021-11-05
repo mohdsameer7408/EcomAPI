d2823bb4c8f9596e8bbc2c151b32d7e7fc3ce425
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2021-11-05 20:36:39 +0530

    completed endpoints for uploading and retreiving images

--- config/db.js
+++ config/db.js
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
+import { GridFsStorage } from "multer-gridfs-storage";
+import multer from "multer";
+import path from "path";
+
+let gfs, upload;
 
 const connectDB = async () => {
   try {
@@ -8,15 +13,39 @@ const connectDB = async () => {
     console.log(`MongoDB connected:${conn.connection.host}`);
 
     // storage connection
-    const gfsConn = await mongoose
+    const fileConn = await mongoose
       .createConnection(DBUri, {
         autoIndex: false,
       })
       .asPromise();
-    console.log(`MongoDB storage connected:${gfsConn.host}`);
+    console.log(`MongoDB storage connected:${fileConn.host}`);
+
+    gfs = new mongoose.mongo.GridFSBucket(fileConn.db, {
+      bucketName: "images",
+    });
+
+    const storage = new GridFsStorage({
+      url: DBUri,
+      options: { useNewUrlParser: true, useUnifiedTopology: true },
+      file: (req, file) =>
+        new Promise((resolve, reject) => {
+          const fileName = `IMG-${Date.now()}${path.extname(
+            file.originalname
+          )}`;
+          const fileInfo = {
+            filename: fileName,
+            bucketName: "images",
+          };
+
+          resolve(fileInfo);
+        }),
+    });
+
+    upload = multer({ storage });
   } catch (error) {
     console.log(`Error while connecting with MongoDB: ${error}`);
   }
 };
 
+export { gfs, upload };
 export default connectDB;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import connectDB from "./config/db.js";
+import connectDB, { gfs, upload } from "./config/db.js";
 
 // configuration
 dotenv.config({ path: "./config/config.env" });
@@ -18,6 +18,24 @@ await connectDB();
 
 // endpoints
 app.get("/", (req, res) => res.status(200).json("Hello world"));
+
+app.post("/api/upload/image", upload.single("file"), async (req, res) =>
+  res.status(201).json(req.file)
+);
+
+app.get("/api/image", async (req, res) => {
+  gfs.find({ filename: req.query.name }).toArray((error, files) => {
+    if (error) {
+      res.status(500).json(error);
+    } else {
+      if (!files[0] || !files.length) {
+        res.status(404).json("File not found!");
+      } else {
+        gfs.openDownloadStreamByName(files[0].filename).pipe(res);
+      }
+    }
+  });
+});
 
 // listener
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
