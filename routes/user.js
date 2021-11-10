231e6f8f1b6cd532a2487934f7c4a2dbb99ad634
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2021-11-10 16:20:05 +0530

    created route for user login and profile update

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import bcrypt from "bcryptjs";
 
 import User from "../models/User.js";
-import { generateHashedPassword, generateToken } from "../config/utils.js";
+import {
+  generateHashedPassword,
+  generateToken,
+  verifyToken,
+} from "../config/utils.js";
 
 const router = Router();
 
@@ -26,6 +30,60 @@ router.post("/register", async (req, res) => {
       _id: createdUser._id,
       userName: createdUser.userName,
       email: createdUser.email,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json(`Something went wrong and an error occured: ${error}`);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const doesUserExists = await User.findOne({ email });
+    if (!doesUserExists)
+      return res.status(400).json(`No user with ${email} try creating one!`);
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      doesUserExists.password
+    );
+    if (!isPasswordValid) return res.status(400).json("Invalid password!");
+
+    const token = generateToken(doesUserExists);
+
+    res.status(200).header("auth-token", token).json({
+      _id: doesUserExists._id,
+      userName: doesUserExists.userName,
+      email: doesUserExists.email,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json(`Something went wrong and an error occured: ${error}`);
+  }
+});
+
+router.patch("/profile/update", verifyToken, async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+    let userData = { userName, email };
+
+    if (password) {
+      const hashedPassword = await generateHashedPassword(password);
+      userData = { ...userData, password: hashedPassword };
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.user._id },
+      userData,
+      { new: true }
+    );
+    const token = req.header("auth-token");
+    res.status(200).header("auth-token", token).json({
+      _id: updatedUser._id,
+      userName: updatedUser.userName,
+      email: updatedUser.email,
       token,
     });
   } catch (error) {
