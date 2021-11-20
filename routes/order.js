21bc4a073f8b95c34f2e62b9d40441e533730d85
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2021-11-20 22:34:09 +0530

    created a route to place order

--- routes/order.js
+++ routes/order.js
@@ -11,7 +11,19 @@ router.get("/orders", verifyToken, async (req, res) => {
       "userId",
       "-password -__v -createdAt -updatedAt"
     );
+
     res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(`Something went wrong and an error occured: ${error}`);
+  }
+});
+
+router.post("/orders/create", verifyToken, async (req, res) => {
+  try {
+    const order = new Order({ ...req.body, userId: req.user._id });
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
   } catch (error) {
     res.status(500).json(`Something went wrong and an error occured: ${error}`);
   }
