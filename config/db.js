c10af148f68ca255818c332bc9bbfa0e32a53730
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2021-11-28 20:08:01 +0530

    made users collection realtime

--- config/db.js
+++ config/db.js
@@ -2,12 +2,23 @@ import mongoose from "mongoose";
 import { GridFsStorage } from "multer-gridfs-storage";
 import multer from "multer";
 import path from "path";
+import Pusher from "pusher";
 
 let gfs, upload;
 
 const connectDB = async () => {
   try {
     const DBUri = `mongodb+srv://root:${process.env.PASS}@cluster0.hltf8.mongodb.net/ecomDB?retryWrites=true&w=majority`;
+
+    // pusher configuration
+    const pusher = new Pusher({
+      appId: "1307350",
+      key: "36fbaf9e90fc929ed231",
+      secret: "8dc1544a6a2fe37009b9",
+      cluster: "mt1",
+      useTLS: true,
+    });
+
     // database connection
     const conn = await mongoose.connect(DBUri, { autoIndex: false });
     console.log(`MongoDB connected:${conn.connection.host}`);
@@ -42,6 +53,27 @@ const connectDB = async () => {
     });
 
     upload = multer({ storage });
+
+    // making mongoDB realtime
+    const userChangeStream = mongoose.connection.collection("users").watch();
+    userChangeStream.on("change", (change) => {
+      console.log("Change stream was triggered!");
+      console.log(change);
+
+      if (change.operationType === "insert") {
+        const data = change.fullDocument;
+        console.log("A user was created!");
+        pusher.trigger("users", "inserted", data);
+      } else if (
+        change.operationType === "update" ||
+        change.operationType === "delete"
+      ) {
+        console.log("A user was updated or deleted!");
+        pusher.trigger("users", "inserted", change.documentKey);
+      } else {
+        console.log("An unknown operation was triggered!");
+      }
+    });
   } catch (error) {
     console.log(`Error while connecting with MongoDB: ${error}`);
   }
